5570cb40dcf3aef5769ada6025be511121ac7351
Anmol Bansal <abanmolbansal5@gmail.com>
2022-07-05 13:35:55 +0530

    Add and Delete

--- movies/settings.py
+++ movies/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)s*+ximhxax%_f8w$@83=uhs&2)k_ei+u@6%xph6=8!_amb&9d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- movies/views.py
+++ movies/views.py
@@ -1,30 +1,64 @@
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.http import Http404
 from django.shortcuts import render
+from .models import Movie
 
-data = {
-    'movies': [
-        {
-            'id': 5,
-            'title': 'Jaws',
-            'year': 1669,
-        },
-        {
-            'id': 6,
-            'title': 'Sharknado',
-            'year': 1660,
-        },
-        {
-            'id': 7,
-            'title': 'The Meg',
-            'year': 1670,
-        }
-    ]
-}
+# data = {
+#     'movies': [
+#         {
+#             'id': 5,
+#             'title': 'Jaws',
+#             'year': 1669,
+#         },
+#         {
+#             'id': 6,
+#             'title': 'Sharknado',
+#             'year': 1660,
+#         },
+#         {
+#             'id': 7,
+#             'title': 'The Meg',
+#             'year': 1670,
+#         }
+#     ]
+# }
 
 
 def movies(req):
-    return render(req, 'movies/movies.html', data)
+    data = Movie.objects.all()
+    return render(req, 'movies/movies.html', {'movies': data})
 
 
 def home(req):
     return HttpResponse("Home Page")
+
+
+def detail(req, id):
+    try:
+        data = Movie.objects.get(pk=id)
+    except:
+        raise Http404('Movie does not exist')
+
+    return render(req, 'movies/detail.html', {'movie': data})
+
+
+def add(req):
+    title = req.POST.get('title')
+    year = req.POST.get('year')
+
+    if title and year:
+        movie = Movie(title=title, year=year)
+        movie.save()
+        return HttpResponseRedirect('/movies')
+    return render(req, 'movies/add.html')
+
+
+def delete(req, id):
+    try:
+        movie = Movie.objects.get(pk=id)
+    except:
+        raise Http404('Movie does not exist')
+
+    movie.delete()
+    return HttpResponseRedirect('/movies')
